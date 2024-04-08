21e08f6fcd3e0fd03af4e16118dd459302e6cbed
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-04-08 21:18:04 +0300

    start HW7 with hw6 files

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,107 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import {
+  addContact,
+  sortContacts,
+  selectContacts,
+} from '../../redux/contactsSlice';
+import css from './ContactForm.module.css';
+
+import { toast } from 'react-hot-toast';
+import { nanoid } from '@reduxjs/toolkit';
+
+const formikValidationShema = Yup.object({
+  name: Yup.string()
+    .min(3, 'Must be at least 3 characters')
+    .max(40, 'Must be max 40 characters or less')
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required('Required'),
+  number: Yup.string()
+    .min(
+      10,
+      'Must be at least 10 digits. If you have 8 or less, add few 0 in front to match format'
+    )
+    .max(20, 'Must be max 20 digits')
+    .matches(
+      /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required('Required'),
+});
+
+const ContactForm = () => {
+  const contacts = useSelector(selectContacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(sortContacts());
+  }, [dispatch]);
+
+  const handleSubmit = (values, actions) => {
+    const tempName = values.name.toLowerCase();
+    const foundContact = contacts.find(
+      cont => cont.name.toLowerCase() === tempName.toLowerCase()
+    );
+    if (foundContact) {
+      toast.error(`${foundContact.name} is already in contact list.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: values.name.trim(),
+      number: values.number.trim(),
+    };
+
+    dispatch(addContact(newContact));
+    dispatch(sortContacts());
+
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        number: '',
+      }}
+      validationSchema={formikValidationShema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={css.contactForm}>
+        <label className={css.contactLabel}>
+          <span className={css.contactInput}>Name</span>
+          <Field className={css.contactInput} type="text" name="name" />
+          <ErrorMessage
+            className={css.contactError}
+            component="p"
+            name="name"
+          />
+        </label>
+        <label className={css.contactLabel}>
+          <span className={css.contactInput}>Number</span>
+          <Field className={css.contactInput} type="text" name="number" />
+          <ErrorMessage
+            className={css.contactError}
+            component="p"
+            name="number"
+          />
+        </label>
+        <div className={css.buttonWrapper}>
+          <button className={css.addBtn} type="submit">
+            Add contact
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux';
+import { selectContacts, deleteContact } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice';
+
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={css.contListArea}>
+      {filteredContacts.map(elm => {
+        return <Contact key={elm.id} deleteContact={deleteContact} {...elm} />;
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
